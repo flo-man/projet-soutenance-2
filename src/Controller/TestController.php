ca37628761c6ae61fa55227c7927a831334d28af
Florent Claudot <flo.brown@me.com>
2020-10-08 16:35:12 +0200

    2eme commit (Test Base Twig)

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,4 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class TestController extends AbstractController
 {
+    /**
+     * @Route ("/")
+     */
+    public function test()
+    {
+        return $this->render('base.html.twig');
+    }
 }
